3b1ff70f7f757fea1c12883ef4bbef2c774fe877
zjdtx <zjdtx11@163.com>
2024-10-22 22:49:38 +0800

    This is a simple project.

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="about">
+        <div >nihao</div>
+        <div class="mt-2 text-3xl font-bold underline">nih123123ao</div>
+    </div>
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped lang="scss">
+
+</style>
